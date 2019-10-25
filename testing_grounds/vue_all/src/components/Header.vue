85e96b9fae3c7195d30e4d7614d592c13c76e59c
devbacon <devon.bacon@gmail.com>
2019-10-25 10:30:03 -0700

    add first custom vue components

---
+++ testing_grounds/vue_all/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="max-width">
+        
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    
+}
+</script>
+
+<style scoped lang="scss">
+    
+</style>
---
+++ testing_grounds/vue_all/src/views/Playground.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="playground">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
