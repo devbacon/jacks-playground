39192ae684dd0cc39a72ac91c4b2ffbfb63208dc
devbacon <devon.bacon@gmail.com>
2020-01-05 12:47:43 -0800

    clear grabage ofbiz framework

--- testing_grounds/ofbiz/applications/accounting/webapp/accounting/WEB-INF/actions/agreement/GetPartyNameForDate.groovy
+++
@@ -1,6 +0,0 @@
-if (partyIdFrom) {
-    context.partyNameResultFrom = runService("getPartyNameForDate", [partyId: partyIdFrom, compareDate: agreementDate, lastNameFirst: "Y"])
-}
-if (partyIdTo) {
-    context.partyNameResultTo = runService("getPartyNameForDate", [partyId: partyIdTo, compareDate: agreementDate, lastNameFirst: "Y"])
-}
